36d9594bbdb341632ebf501e6f23e12109d4ed52
shaoshuai <shaoshuai@qiyi.com>
2024-06-04 17:06:28 +0800

    add goroutine

--- proxy.go
+++ proxy.go
@@ -7,17 +7,32 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"net/http/httputil"
 	"net/url"
 	"sync"
+	"time"
 )
 
 // NewProxy creates a new reverse proxy to the target host
 func NewProxy(targetHost string) *httputil.ReverseProxy {
 	target, _ := url.Parse(targetHost)
 	proxy := httputil.NewSingleHostReverseProxy(target)
+	proxy.Transport = &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		MaxIdleConns:          500,
+		MaxIdleConnsPerHost:   100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   3 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+
 	return proxy
 }
 
